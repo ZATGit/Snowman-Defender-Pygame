d45d44c1621addb6be312a1eb0bd1403ada093f6
ZATGit <zachtrembly@gmail.com>
2019-03-10 18:19:42 -0400

    Added Game class: Streamline game logic and allow game to easily restart in game_events method.

--- snowman_brick_collector.py
+++ snowman_brick_collector.py
@@ -7,11 +7,129 @@ BLUE = (0, 0, 255)
 RED = (255, 0, 0)
 GREEN = (0, 255, 0)
 
-# Screen Dimensions
+#Screen Dimensions
+#Fix Constants Pep-8; Caps
 screen_width = 700
 screen_height = 400
-screen = pygame.display.set_mode([screen_width, screen_height])
-pygame.display.set_caption("Save Snowy Village!")
+
+class Game(object):
+    """Contains the game logic, events, and display."""
+
+    def __init__(self):
+        """Constructs block lists, blocks, and game conditions."""
+        self.score = 0
+        self.game_over = False
+        self.good_block_list = pygame.sprite.Group()
+        self.bad_block_list = pygame.sprite.Group()
+        self.all_sprites_list = pygame.sprite.Group()
+
+
+        #Good Block List Creation
+        for i in range(35):
+            # Block Image
+            block = Block("snowman_sprite.png")
+            # Block Locations
+            block.rect.x = random.randrange(screen_width)
+            block.rect.y = random.randrange(screen_height)
+            # Add Blocks to Lists
+            self.good_block_list.add(block)
+            self.all_sprites_list.add(block)
+
+        #Bad Block List Creation
+        for i in range(25):
+            block = Block("robot_sprite.png")
+            block.rect.x = random.randrange(screen_width)
+            block.rect.y = random.randrange(screen_height)
+            self.bad_block_list.add(block)
+            self.all_sprites_list.add(block)
+
+        self.player = Player()
+        self.all_sprites_list.add(self.player)
+
+    def display_screen(self,screen):
+
+        # background_position = [0, 0]
+        background_image = pygame.image.load("snowy_village.png").convert()
+
+        screen.blit(background_image, [0, 0])
+
+        if self.game_over:
+            screen.fill(BLACK)
+            font = pygame.font.SysFont("Courier",30)
+            text = font.render("Game Over. \nHit Space to Play Again", True, WHITE)
+            center_text_x = (screen_width // 2) - (text.get_width() // 2)
+            center_text_y = (screen_height // 2) - (text.get_height() // 2)
+            screen.blit(text, [center_text_x, center_text_y])
+
+        if not self.game_over:
+            self.all_sprites_list.draw(screen)
+
+        pygame.display.flip()
+
+
+    def game_events(self):
+        """Contains Game Processes & Events (Controls)."""
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return True
+            if event.type == pygame.K_SPACE:
+                if self.game_over:
+                    self.__init__()
+
+            #User Presses Key Down
+            elif event.type == pygame.KEYDOWN:
+                #which key and adjust speed
+                if event.key == pygame.K_a:
+                    player.changespeed(-3,0)
+                elif event.key == pygame.K_d:
+                    player.changespeed(3,0)
+                elif event.key == pygame.K_w:
+                    player.changespeed(0,-3)
+                elif event.key == pygame.K_s:
+                    player.changespeed(0,3)
+
+            #User Stops Pressing Key
+            elif event.type == pygame.KEYUP:
+                if event.key == pygame.K_a:
+                    player.changespeed(3,0)
+                elif event.key == pygame.K_d:
+                    player.changespeed(-3,0)
+                elif event.key == pygame.K_w:
+                    player.changespeed(0,3)
+                elif event.key == pygame.K_s:
+                    player.changespeed(0,-3)
+
+        return False
+
+    def game_logic(self):
+        """Updates block positions and checks for block collisions."""
+        if not self.game_over:
+            self.all_sprites_list.update()
+
+            # Block Collision Sounds
+            good_block_sound = pygame.mixer.Sound("good_block.wav")
+            bad_block_sound = pygame.mixer.Sound("bad_block.wav")
+
+            #Check for Collisions between player and Other Blocks
+            good_blocks_hit_list = pygame.sprite.spritecollide(self.player, self.good_block_list, True)
+            bad_blocks_hit_list = pygame.sprite.spritecollide(self.player, self.bad_block_list, True)
+
+            #Good Collisions
+            for block in good_blocks_hit_list:
+                self.score += 1
+                good_block_sound.play()
+                print(self.score)
+
+            #Bad Collisions
+            for block in bad_blocks_hit_list:
+                self.score -= 1
+                bad_block_sound.play()
+                print(self.score)
+
+            #Game Over Condition
+            if len(self.good_block_list) == 35:
+                self.game_over = True
+
 
 class Block(pygame.sprite.Sprite):
    """Creates the attributes for all blocks."""
@@ -22,6 +140,9 @@ class Block(pygame.sprite.Sprite):
         self.image.set_colorkey(WHITE)
         #Position Update, Setting rect.x and rect.y
         self.rect = self.image.get_rect()
+        #Block Collision Sounds
+        self.good_block_sound = pygame.mixer.Sound("good_block.wav")
+        self.bad_block_sound = pygame.mixer.Sound("bad_block.wav")
 
 class Player(pygame.sprite.Sprite):
     """ Create's player controlled block."""
@@ -31,7 +152,7 @@ class Player(pygame.sprite.Sprite):
         #Height,Width,Color
         self.image = pygame.Surface([15, 15])
         self.image.fill(BLUE)
-        # Pass-in Location
+        #Pass-in Location
         self.rect = self.image.get_rect()
         self.rect.x = x
         self.rect.y = y
@@ -67,111 +188,40 @@ class Player(pygame.sprite.Sprite):
         if self.rect.y > (400 - 15):
             self.rect.y = (400 - 15)
 
+
 def main():
 
     pygame.init()
 
-    background_position = [0, 0]
-    background_image = pygame.image.load("snowy_village.png").convert()
-
-    #Game Sounds
-    #Good Sound
-    good_block_sound = pygame.mixer.Sound("good_block.wav")
-    #Bad Sound
-    bad_block_sound = pygame.mixer.Sound("bad_block.wav")
-
-
-    #Sprite Lists
-    good_block_list = pygame.sprite.Group()
-    bad_block_list = pygame.sprite.Group()
-    all_sprites_list = pygame.sprite.Group()
-
-    #Good Block List Creation
-    for i in range(35):
-        #Block Image
-        block = Block("snowman_sprite.png")
-        #Block Locations
-        block.rect.x = random.randrange(screen_width)
-        block.rect.y = random.randrange(screen_height)
-        #Add Blocks to Lists
-        good_block_list.add(block)
-        all_sprites_list.add(block)
-
-    #Bad Block List Creation
-    for i in range(25):
-        block = Block("robot_sprite.png")
-        block.rect.x = random.randrange(screen_width)
-        block.rect.y = random.randrange(screen_height)
-        bad_block_list.add(block)
-        all_sprites_list.add(block)
-
-    #Player Block
-    player = Player(100,100)
-    all_sprites_list.add(player)
+    screen = pygame.display.set_mode([screen_width, screen_height])
+    pygame.display.set_caption("Save Snowy Village!")
 
     done = False
 
     clock = pygame.time.Clock()
 
-    frame_count = 0
-    frame_rate = 60
-    start_time = 90
+    game = Game()
 
-    score = 0
-
-    #Main Game Loop
     while not done:
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                done = True
-            #User Presses Key Down
-            elif event.type == pygame.KEYDOWN:
-                #which key and adjust speed
-                if event.key == pygame.K_a:
-                    player.changespeed(-3,0)
-                elif event.key == pygame.K_d:
-                    player.changespeed(3,0)
-                elif event.key == pygame.K_w:
-                    player.changespeed(0,-3)
-                elif event.key == pygame.K_s:
-                    player.changespeed(0,3)
-            #User Stops Pressing Key
-            elif event.type == pygame.KEYUP:
-                if event.key == pygame.K_a:
-                    player.changespeed(3,0)
-                elif event.key == pygame.K_d:
-                    player.changespeed(-3,0)
-                elif event.key == pygame.K_w:
-                    player.changespeed(0,3)
-                elif event.key == pygame.K_s:
-                    player.changespeed(0,-3)
 
-        all_sprites_list.update()
+        done = game.game_events()
 
-        screen.blit(background_image, [0,0])
 
-        #Check for Collisions between player and Other Blocks
-        good_blocks_hit_list = pygame.sprite.spritecollide(player, good_block_list, True)
-        bad_blocks_hit_list = pygame.sprite.spritecollide(player, bad_block_list, True)
+        game.game_logic()
 
-        #Check for Good Collisions
-        for block in good_blocks_hit_list:
-            #add 1 to score if collision
-            score += 1
-            good_block_sound.play()
-            print(score)
 
-        #Check for bad Collisions
-        for block in bad_blocks_hit_list:
-            #subtract 1 from score if collision
-            score -= 1
-            bad_block_sound.play()
-            print(score)
+        game.display_screen(screen)
 
-        all_sprites_list.draw(screen)
+
+        #frame_count = 0
+        #frame_rate = 60
+        #start_time = 90
+
+
+        #Main Game Loop
 
         #Score
-        font = pygame.font.SysFont('Courier', 25)
+        """font = pygame.font.SysFont('Courier', 25)
         scoretext = font.render("Score:" + str(score), True, BLACK)
         screen.blit(scoretext, [10, 10])
 
@@ -185,9 +235,9 @@ def main():
 
         displayed_string = "Time left: {0:02}:{1:02}".format(minutes, seconds)
         text = font.render(displayed_string, True, BLACK)
-        screen.blit(text, [450, 10])
+        screen.blit(text, [450, 10])"""
 
-        frame_count += 1
+        #frame_count += 1
 
         pygame.display.flip()
 
